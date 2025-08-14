67acde722f02ddbef53162450c138695f15253ff
Anzor <boriev770@gmail.com>
2025-08-14 23:20:02 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ TOKEN = "7559588518:AAEv5n_8N_gGo97HwpZXDHTi3EQ40S1aFcI"
 
 # FAQ: ключевые слова (regex) -> ответ
 FAQ = {
-    r"(Эльдар*)": "Э, динахуй",
+    r"(сдела.*|заказ.*)": "Для заказа напишите нам в телеграм @probuykmvadmin"
 }
 
 bot = Bot(token=TOKEN)
@@ -18,7 +18,7 @@ dp = Dispatcher(bot)
 async def start(message: types.Message):
     await message.answer(
         "Привет! Напиши свой вопрос, а я постараюсь найти на него ответ.\n"
-        "Например: 'Сколько стоит доставка?' или 'Где вы находитесь?'"
+        "Например: 'Сколько стоит доставка?' или 'Как сделать заказ?'"
     )
 
 @dp.message_handler()
